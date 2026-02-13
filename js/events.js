7371abee66b377ac045e72a4d9712c92201a284e
vdhishah2010-byte <vdhishah2010@gmail.com>
2026-02-13 16:10:43 +0530

    Initial diary version

---
+++ js/events.js
@@ -0,0 +1,14 @@
+const events = [
+  {
+    title: "Our First Coffee",
+    images: ["events/event1/img1.jpg", "events/event1/img2.jpg"],
+    captionMe: "You were nervous but pretending not to be.",
+    captionPartner: "You smiled and I knew this was different."
+  },
+  {
+    title: "Beach Sunset",
+    images: ["events/event2/img1.jpg"],
+    captionMe: "The sky was jealous of us that day.",
+    captionPartner: "I didn’t want the sun to set."
+  }
+];
